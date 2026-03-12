fa510b32e00d90188ce8fc9fbd0701ee0e19ecb7
Wang yizhang <yizhangwang07@gmail.com>
2026-03-13 00:20:32 +0800

    Added benchmark function for aruco detection & solvePnP. Added rosargs for aruco size & benchmark

--- rpi/aruco_detector/aruco_detector/aruco.py
+++ rpi/aruco_detector/aruco_detector/aruco.py
@@ -11,6 +11,7 @@ import cv2
 import numpy as np
 
 import tf2_ros
+import time
 
 """
 [FIX] 
@@ -44,8 +45,12 @@ class ArucoDetector(Node):
         # Command line variables
         self.declare_parameter('verbouse', False)
         self.declare_parameter('frequency', 20)
+        self.declare_parameter('marker_size', 0.05)
+        self.declare_parameter('benchmark', False)
         self.verbouse = self.get_parameter('verbouse').get_parameter_value().bool_value
         self.update_frequency = self.get_parameter('frequency').get_parameter_value().integer_value
+        self.marker_length = self.get_parameter('marker_size').get_parameter_value().double_value
+        self.benchmark = self.get_parameter('benchmark').get_parameter_value().bool_value
 
         # QoS profile for camera images
         qos_profile = QoSProfile(
@@ -79,9 +84,6 @@ class ArucoDetector(Node):
         calib_data = np.load("/home/grp5/turtlebot3_ws/src/aruco_detector/aruco_detector/camera_calib.npz")
         self.camera_matrix = calib_data['camera_matrix']
         self.dist_coeffs = calib_data['dist_coeff']
-
-        # Real-world marker size in meters
-        self.marker_length = 0.05
 
         # Frame buffer & msg stamp
         self.frame_buffer = None
@@ -119,8 +121,6 @@ class ArucoDetector(Node):
         Compute & publish TF transform of aruco_marker_id -> camera_link
     Otherwise:
         Does nothing
-
-    Also why are we trying to publish pose?
     """
     def locate_aruco(self):
         if self.frame_buffer is None or self.msg_stamp is None:
@@ -131,15 +131,23 @@ class ArucoDetector(Node):
         stamp = self.msg_stamp
 
         # Detect Aruco markers
+        if self.benchmark:
+            timestamp = time.perf_counter()
         corners, ids, _ = cv2.aruco.detectMarkers(
             frame,
             self.dictionary,
             parameters=self.parameters
         )
 
+        if self.benchmark:
+            aruco_detect_time = time.perf_counter() - timestamp
+
         if ids is not None:
             for i in range(len(ids)):
                 # Solve PnP for marker pose
+                if self.benchmark:
+                    timestamp = time.perf_counter()
+
                 success, rvec, tvec = cv2.solvePnP(
                     self.obj_points,
                     corners[i],
@@ -148,6 +156,8 @@ class ArucoDetector(Node):
                     flags=cv2.SOLVEPNP_IPPE_SQUARE
                 )
 
+                if self.benchmark:
+                    pnp_time = time.perf_counter() - timestamp
                 if success:
                     # Convert rotation vector to rotation matrix
                     # rot_matrix, _ = cv2.Rodrigues(rvec)
@@ -165,7 +175,7 @@ class ArucoDetector(Node):
                     # Translation
                     # Transform camera coord. sys to match turtlebot
                     t.transform.translation.x = float(tvec[2][0])
-                    t.transform.translation.y = -float(tvec[0][0])
+                    t.transform.translation.y = float(tvec[0][0])
                     t.transform.translation.z = -float(tvec[1][0])
 
                     # Rotation
@@ -180,7 +190,8 @@ class ArucoDetector(Node):
                     # Log info if verbouse
                     if self.verbouse:
                         self.get_logger().info(f'Marker ID:{ids[i][0]} found, x:{float(tvec[0][0])}, y:{float(tvec[1][0])}, z{float(tvec[2][0])}')
-                
+                    if self.benchmark:
+                        self.get_logger().info(f"Detection ms: {aruco_detect_time * 1000}, solvePnP ms: {pnp_time * 1000}")
                 elif self.verbouse:
                     self.get_logger().info(f"Marker ID: {ids[i][0]} found, solvePnP failed.")
 
